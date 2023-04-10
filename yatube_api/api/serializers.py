820c4a43000854656debb088678f5a6650be9a11
Роман Беспалов <rbespalov.97@inbox.ru>
2023-04-10 22:01:26 +0400

    First version

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.fields import CurrentUserDefault
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -17,7 +18,43 @@ class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('author', 'post')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True, slug_field='username', default=CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username', read_only=False, queryset=User.objects.all()
+    )
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+        validators = (
+            serializers.UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message='Вы уже подписались на этого пользователя'
+            ),
+        )
+
+    def validate(self, attrs):
+        """Нельзя подписываться на себя"""
+        if self.context['request'].user == attrs['following']:
+            raise serializers.ValidationError(
+                'Вы не можете подписаться на самого себя'
+            )
+        return attrs
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Group
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,66 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, mixins
+from rest_framework import filters, permissions
+from rest_framework.pagination import LimitOffsetPagination
+
+from posts.models import Comment, Group, Post, User
+from api.serializers import (
+    CommentSerializer,
+    GroupSerializer,
+    FollowSerializer,
+    PostSerializer
+)
+from api.permissions import AuthorOrReadonly
+
+
+class FollowViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.request.user)
+        following = user.follower.all()
+        return following
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (
+        AuthorOrReadonly, permissions.IsAuthenticatedOrReadOnly,
+    )
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (
+        AuthorOrReadonly, permissions.IsAuthenticatedOrReadOnly
+    )
+
+    def get_post_id(self):
+        return self.kwargs.get("post_id")
+
+    def get_queryset(self):
+        new_queryset = Comment.objects.filter(post=self.get_post_id())
+        return new_queryset
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.get_post_id())
+        serializer.save(author=self.request.user, post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
